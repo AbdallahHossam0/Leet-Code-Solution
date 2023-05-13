40e2431719feb16b1890780a09583f40edecb1e1
Abdallah Hossam-Eldin <eng.abdallahhossam@outlook.com>
2023-05-12 19:47:12 -0700

    Add solutions for Find The Original Array of Prefix Xor Problem

---
+++ Find The Original Array of Prefix Xor/Solutions/solution1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> ans(pref.size());
+        ans[0] = pref[0];
+        for(size_t i{1}; i < pref.size(); ++i){
+            ans[i] = pref[i] ^ pref[i - 1];
+        }
+        return ans;
+    }
+};
